44642653f080e66cd430597b5e2f8b4a835fa826
Vini <viniciusicmsc@gmail.com>
2024-11-07 16:56:02 -0300

    restart

--- backend/attachments/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from attachments.models import FileAttachments, AudioAttachments
-
-admin.site.register(FileAttachments)
-admin.site.register(AudioAttachments)
--- backend/core/socket.py
+++
@@ -1,6 +0,0 @@
-import socketio
-from django.conf import settings
-
-sio = socketio.Server(
-  cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS
-)
