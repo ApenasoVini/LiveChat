dc8044de96f8f891a492707d1ed9f42863f6f4d7
Vini <viniciusicmsc@gmail.com>
2024-11-08 17:12:24 -0300

    livechat

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import socketio
+import eventlet
+import eventlet.wsgi
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from core.socket import sio 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
+application = socketio.WSGIApp(sio, application)
+
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
