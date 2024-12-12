0723c7b6443e2f0548b21f76ccc663450bef217c
Vini <viniciusicmsc@gmail.com>
2024-12-12 16:55:50 -0300

    Frontend

--- frontend/src/lib/schemas/chatSchema.ts
+++ frontend/src/lib/schemas/chatSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const newChatSchema = z.object({
-    email: z.string().email({ message: 'Email inválido' })
-})
+  email: z.string().email({ message: "Email inválido" }),
+});
 
-export type NewChatData = z.infer<typeof newChatSchema>
+export type NewChatData = z.infer<typeof newChatSchema>;
