17b80ddee4f8ba27cd79cc0899a377a80acb5e09
Vini <viniciusicmsc@gmail.com>
2024-12-14 16:13:05 -0300

    Frontend

--- frontend/src/components/ui/avatar.tsx
+++ frontend/src/components/ui/avatar.tsx
@@ -4,33 +4,33 @@ import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 import { cn } from "@/lib/utils";
-
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & {
+    isOnline?: boolean;
+  } // Adicionando isOnline aqui
+>(({ className, isOnline = false, ...props }, ref) => (
+  <div className="relative">
+    {isOnline && (
+      <div className="absolute bg-emerald-400 z-10 rounded-full border border-slate-300 size-3 bottom-0.5 right-0" />
     )}
-    {...props}
-  />
+    <AvatarPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+        className
+      )}
+      {...props}
+    />
+  </div>
 ));
 Avatar.displayName = AvatarPrimitive.Root.displayName;
 
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image> & {
-    isOnline?: boolean;
-  }
->(({ className, isOnline = false, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
   <div className="relative">
-    {isOnline && (
-      <div className="absolute bg-emerald-400 z-10 rounded-full border-slate-300 size-3 bottom-0.5 right-0" />
-    )}
-
     <AvatarPrimitive.Image
       ref={ref}
       className={cn("aspect-square h-full w-full", className)}
